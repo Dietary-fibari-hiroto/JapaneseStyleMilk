4eab26cdb6de250c83ee14666d289e39e53027d9
mod0dethink <tanakaryuto1994@gmail.com>
2025-06-13 09:58:42 +0900

    Express初期設定

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
-console.log("しゅうゆ");
+// server.ts
+import express from 'express';
+
+const app = express();
+
+// ミドルウェアの設定
+app.use(express.json());
+
+// サーバー起動
+app.listen(3000, () => {
+  console.log('サーバー起動');
+});
