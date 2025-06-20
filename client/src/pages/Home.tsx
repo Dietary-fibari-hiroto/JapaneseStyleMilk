8229a3cc600cdbcfca24571b7639f49c6ab74f97
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-06-20 16:43:16 +0900

    save:

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,9 @@
 const Home = () => {
   return (
-<<<<<<< HEAD
     // <div className="w-[100vw] h-screen flex justify-center items-center text-[100px]">
     //   和風牛乳
     // </div>
     <div></div>
-=======
-    <div className="w-[100vw] h-screen flex justify-center items-center text-[100px]">
-      和風牛乳
-    </div>
->>>>>>> origin/develop
   );
 };
 
