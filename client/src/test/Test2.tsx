2aaefa4d42db4672ee2f8b849d7daa25209a620b
Haruki <k.haruki.zephyranthes@gmail.com>
2025-06-20 11:53:24 +0900

    task:UserCardコンポーネント作成

--- client/src/test/Test2.tsx
+++ client/src/test/Test2.tsx
@@ -4,8 +4,19 @@ import UserCard from "../components/common/UserCard";
 const Test2 = () => {
   return (
     <div className="flex flex-col space-y-2">
+      {/* UserCard */}
+      <UserCard color="red" />
+      <UserCard color="yellow" />
+      <UserCard color="orange" />
+      <UserCard color="green" />
+      <UserCard color="blue" />
+      <UserCard color="purple" />
+      <UserCard color="pink" />
+      <UserCard color="white" />
+      <UserCard color="black" />
+
       {/* PrimaryButton */}
-      <p className="text-[2rem]">Middium</p>
+      {/* <p className="text-[2rem]">Middium</p>
       <PrimaryButton size="Meddium" color="Main" shape="Round" label="Home"/>
       <PrimaryButton size="Meddium" color="Main" shape="Square" label="Test1"/>
       <PrimaryButton size="Meddium" color="Sub" shape="Round" label="アップグレード"/>
@@ -15,7 +26,7 @@ const Test2 = () => {
       <PrimaryButton size="Large" color="Main" shape="Round" label="アップグレード"/>
       <PrimaryButton size="Large" color="Main" shape="Square" label="アップグレード"/>
       <PrimaryButton size="Large" color="Sub" shape="Round" label="アップグレード"/>
-      <PrimaryButton size="Large" color="Sub" shape="Square" label="アップグレード"/>
+      <PrimaryButton size="Large" color="Sub" shape="Square" label="アップグレード"/> */}
     </div>
   )
 };
