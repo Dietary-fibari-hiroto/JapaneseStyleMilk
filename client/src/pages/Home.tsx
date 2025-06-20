30cc68522406146841cc20dd989d7d0cdbf6121d
Haruki <k.haruki.zephyranthes@gmail.com>
2025-06-20 11:43:23 +0900

    task:PrimaryButtonのスタイルを変数指定に変更

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import Button from "../components/common/Button";
-
 const Home = () => {
   return (
-    // <div className="w-[100vw] h-screen flex justify-center items-center text-[100px]">
-    //   和風牛乳
-    // </div>
-    <Button size="Meddium" color="Main" shape="Round" label="アップグレード"/>
+    <div className="w-[100vw] h-screen flex justify-center items-center text-[100px]">
+      和風牛乳
+    </div>
   );
 };
 
