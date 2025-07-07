c14630adea280fdc301190e3117caef10e129e3d
Haruki <k.haruki.zephyranthes@gmail.com>
2025-07-07 13:43:22 +0900

    task:ダッシュボード配置

--- client/src/components/feature/debate/ScoreCard.tsx
+++ client/src/components/feature/debate/ScoreCard.tsx
@@ -1,13 +1,14 @@
 type Props = {
   title: string;
   score: number;
+  category: string;
 };
-const ScoreCard = ({ title, score }: Props) => {
+const ScoreCard = ({ title, score, category }: Props) => {
   return (
-    <div className="size-[200px] flex justify-end  bg-[--color-pink-500] rounded-[8px] text-[--text-card-header]">
-      <div className="flex flex-col justify-between h-full w-[195px] bg-[--surface-score_card] rounded-[8px] p-[16px]">
-        <p className="text-[24px]">論理性{title}</p>
-        <p className="text-end text-[40px] font-bold">{score}</p>
+    <div className={`size-[200px] flex justify-end rounded-[8px] text-[--text-card-header]`} style={{backgroundColor: `var(--border-score_card-${category})`}}>
+      <div className="flex flex-col justify-between h-full w-[195px] bg-[--surface-score_card] rounded-[8px] pt-[10px] pb-2 pl-6 pr-4">
+        <p className="text-[24px] font-medium">{title}</p>
+        <p className="text-end text-[40px] font-bold">{score}点</p>
       </div>
     </div>
   );
--- client/src/pages/userInfo/DashBoardPage.tsx
+++ client/src/pages/userInfo/DashBoardPage.tsx
@@ -1,10 +1,18 @@
-import { DebateResultDataCard, EvaluationCard, MonthSelector } from "../../components";
+import { DebateResultDataCard, EvaluationCard, MonthSelector, ScoreCard } from "../../components";
+
+// テスト用総合評価
+const evaluationItems = [
+  { category:"logic", label: "論理性", score: 25},
+  { category:"composition", label: "構成力", score: 25},
+  { category:"argument", label: "反論力", score: 25},
+  { category:"proficiency", label: "英語力", score: 25},
+]
 
 const DashBoardPage = () => {
   return(
-    <>
+    <div className="flex flex-col gap-8">
       {/* 月別グラフ・勝敗数 */}
-      <div className="flex w-full border mb-8">
+      <div className="flex w-full border">
         <div className="w-[80%]">
           <MonthSelector/>
           <p>グラフ差し替え</p>
@@ -15,11 +23,25 @@ const DashBoardPage = () => {
         </div>
       </div>
 
-      {/*  */}
-      <div></div>
-      
+      {/* 総合評価 */}
+      <div className="w-full flex flex-col">
+        <p className="text-body-r font-bold">総合評価</p>
+        <div className="w-full flex gap-5">{
+          evaluationItems.map((element) => {
+            return(
+              <ScoreCard
+                key={element.category}
+                title={element.label}       // 表示文字列
+                score={element.score}       // 点数
+                category={element.category} // 評価分類(色指定用)
+              />
+            )
+          })
+        }</div>
+      </div>
+
       <EvaluationCard debateTitle="title" userImg1="" userImg2="" date="" />
-    </>
+    </div>
   )
 };
 
