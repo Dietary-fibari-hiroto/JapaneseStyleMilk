45937fd9844914982b354af1d2896a9f8f8f3afb
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-07-14 16:16:10 +0900

    save

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = () => {
     <div className="w-screen h-screen flex-all-center">
       <div className="w-[90vw] h-[90lvh] flex  justify-between items-center drop-shadow-2xl bg-white rounded-[24px] px-[100px]">
         <Outlet />
-        <section className="w-[624px] h-[640px] rounded-[24px] bg-[#cccccc] flex-all-center">
+        <section className="w-[624px] h-[80lvh] rounded-[24px] bg-[#cccccc] flex-all-center">
           <img className="w-1/2" src={ImagesRoute.logo_icon} />
         </section>
       </div>
